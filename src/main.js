32a022c7efd23be186b8a63ac2298a101402e9ac
grittyyy <grittyyy@outlook.com>
2023-06-08 21:48:11 +0500

    исправления ошибок

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
-import DefaultMarkupPresenter from './presenter/trip-presenter.js';
+import HeaderPresenter from './presenter/header-presenter.js';
+import TripPresenter from './presenter/trip-presenter.js';
 
-const markup = new DefaultMarkupPresenter();
 
-markup.createToolsMarkup();
-markup.createPointsMarkup();
+const tripMainElement = document.querySelector('.trip-main');
+const headerPresenter = new HeaderPresenter(tripMainElement);
+
+const eventElement = document.querySelector('.trip-events');
+const eventPresenter = new TripPresenter(eventElement);
+
+headerPresenter.init();
+eventPresenter.init();
