1c724c3f9910374e80515b9b90a4dbf1672a8429
grittyyy <grittyyy@outlook.com>
2023-06-13 23:29:27 +0500

    Удалил ненужные файлы

--- src/src/mock/filter.js
+++
@@ -1,9 +0,0 @@
-import { filter } from '../utils/filter';
-
-const generateFilters = (events) => Object.entries(filter).map(
-  ([filterType, filterEvents]) => ({
-    type: filterType,
-    count: filterEvents(events).length,
-  }));
-
-export {generateFilters};
